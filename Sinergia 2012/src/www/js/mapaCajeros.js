7ae15b971a754fdaa5bc703020226dd86a7b42a4
Alejandro Szenkier <aleszenk@hotmail.com>
2013-01-07 04:04:44 -0200

    Base de datos agregada y funcionando. Imagenes cargadas desde base de datos. Se arreglaron estilos y navegabilidad de varias paginas, sobre todo las paginas de los filtros.

--- Sinergia 2012/src/www/js/mapaCajeros.js
+++ Sinergia 2012/src/www/js/mapaCajeros.js
@@ -1,18 +1,4 @@
-/*function displayCajeros(){
-    //lo centro en mi apto
-    var initialLocation = new google.maps.LatLng('-34.90530797754054', '-56.18638873100281');
-    var myOptions = {
-        zoom: 16,
-        center: initialLocation,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvasCajeros"), myOptions);
-    
-    addMarkersToMapCajeros(map);
-    
-}*/
-
-var map, GeoMarker;
+var map, GeoMarker, userPosition;
 
 function displayCajeros() {
 
@@ -31,8 +17,8 @@ function displayCajeros() {
 	});
 
 	google.maps.event.addListenerOnce(GeoMarker, 'position_changed',
-			function() {
-				map.setCenter(this.getPosition());
+			function() {	
+				map.setCenter(this.getPosition());				
 				map.fitBounds(this.getBounds());
 			});
     	
@@ -40,28 +26,30 @@ function displayCajeros() {
 		alert('There was an error obtaining your position. Message: '
 				+ e.message);
 	});
-	
-	addMarkersToMapCajeros(map);
+	/*var mapBounds = new google.maps.LatLngBounds();
+	mapBounds.extend(map.center);*/
+	var cajeros = addMarkersToMapCajeros(map);
 	GeoMarker.setMap(map);
+	/*for (i = 0; i < cajeros.EnvioCajerosResult.length; i++) {
+		var latitudeAndLongitudeOne = new google.maps.LatLng(cajeros.EnvioCajerosResult[i].Latitud,cajeros.EnvioCajerosResult[i].Longitud);
+		mapBounds.extend(latitudeAndLongitudeOne);
+	}
+	map.fitBounds(mapBounds);*/
 	$(window).resize(function() {
 		google.maps.event.trigger(map, 'resize');
-
 	});
-
 }
 
-function addMarkersToMapCajeros(map) {
-
-	var mapBounds = new google.maps.LatLngBounds();
+function addMarkersToMapCajeros(map) {	
+	var cajerosResult;
 	$.getJSON('http://hp-hp/Sinergia2012/ServicioSinergia.svc/cajeros?callback=?',
 					null,
 					function(cajeros) {
+					cajerosResult=cajeros;
 						for (i = 0; i < cajeros.EnvioCajerosResult.length; i++) {
-
 							var latitudeAndLongitudeOne = new google.maps.LatLng(
 									cajeros.EnvioCajerosResult[i].Latitud,
 									cajeros.EnvioCajerosResult[i].Longitud);
-							mapBounds.extend(latitudeAndLongitudeOne);
 							var marker1 = new google.maps.Marker({
 								position : latitudeAndLongitudeOne,
 								map : map
@@ -89,12 +77,11 @@ function addMarkersToMapCajeros(map) {
 										mostrarDetallesCajero(detalles, map,
 												marker1);
 									});
-
+							//mapBounds.extend(latitudeAndLongitudeOne);
 						}
 					});
 	// esto es para que ajuste el zoom segun los marcadores
-	map.fitBounds(mapBounds);
-	// google.maps.event.trigger(map, 'resize');
+	return cajerosResult;
 }
 function errorCajero() {
 	alert('Algun error');
